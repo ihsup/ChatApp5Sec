a501dd0f236bf4c60e16c0dc1d54773a6583e2d9
ihsup <ishup4063@gmail.com>
2025-01-20 12:13:51 +0530

    Chat App
    Connect with friends, family, and the world in real time!
    Messages that bring you closer—instant and secure.
    Never miss a conversation, chat anytime, anywhere.
    Your words, your connection, your chat—anytime, anywhere.
    Stay in touch effortlessly, no matter where you are.
    Chat made simple—because your conversations matter.
    Bringing you and your loved ones closer, one message at a time.
    Experience the future of communication—instant, secure, and easy.
    Instant messaging, endless possibilities—talk more, connect more.
    Real-time chat, limitless conversations, secure connections.

---
+++ client.js
@@ -0,0 +1,59 @@
+const socket = io()
+let name;
+let textarea = document.querySelector('#textarea')
+let messageArea = document.querySelector('.message__area')
+
+do {
+    name = prompt('Please enter your name: ')
+} while(!name)
+
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter') {
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(message) {
+    let msg = {
+        user: name,
+        message: message.trim()
+    }
+    // Append 
+    appendMessage(msg, 'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+
+    // Send to server 
+    socket.emit('message', msg)
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv)
+
+    // After 10 seconds, remove the message with animation
+    setTimeout(() => {
+        mainDiv.classList.add('fade-out')
+        setTimeout(() => {
+            mainDiv.remove()
+        }, 1000); // Wait for fade-out animation to finish
+    }, 10000) // 10 seconds
+}
+
+// Recieve messages 
+socket.on('message', (msg) => {
+    appendMessage(msg, 'incoming')
+    scrollToBottom()
+})
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
+}
